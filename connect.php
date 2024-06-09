cebc613d93c9e1329880da7d39c7f70f4a529b39
Tech-Hiring-24-Technology <sonarsuresh1299@gmail.com>
2024-06-09 10:32:18 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+$name= $_POST['email'];
+$password=$_POST['password'];
+
+
+
+
+
+$conn = new mysqli('localhost', 'root','', 'test');
+if($conn->connect_error) {
+die('Connection Failed : $conn->connect_error);
+}else{
+$stmt $conn->prepare("insert into login(email,password)
+values(?, ?)");
+$stmt->bind_param("ss",$email, $password);
+$stmt->execute();
+echo "registration Successfully...";
+$stmt->close();
+$conn->close();
+}
+?>
