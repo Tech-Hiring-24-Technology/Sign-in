736e0550236e3ab66b4c8a283ef752db7de000d3
Tech-Hiring-24-Technology <sonarsuresh1299@gmail.com>
2024-06-11 10:40:50 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,21 +1,3 @@
 <?php
-$name= $_POST['email'];
-$password=$_POST['password'];
-
-
-
-
-
-$conn = new mysqli('localhost', 'root','', 'test');
-if($conn->connect_error) {
-die('Connection Failed : $conn->connect_error);
-}else{
-$stmt $conn->prepare("insert into signin(email,password)
-values(?, ?)");
-$stmt->bind_param("ss",$email, $password);
-$stmt->execute();
-echo "registration Successfully...";
-$stmt->close();
-$conn->close();
-}
+$echo"shubham";
 ?>
